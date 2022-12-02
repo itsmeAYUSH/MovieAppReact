5a4475eea62a3f9370f2ba6fd9c6c1a1cff5e634
Ayush Sagar <70971035+itsmeAYUSH@users.noreply.github.com>
2022-12-03 00:04:09 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -20,32 +20,64 @@ function App() {
   // ];
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [isTrying , setIsTrying ] = useState(false);
+  let timeoutID;
 
   async function fetchMovieHandler() {
     setIsLoading(true);
-    const response = await fetch("https://swapi.py4e.com/api/films/");
-    const data = await response.json();
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.py4e.com/api/film/");
+
+      if (!response.ok) {
+        throw new Error("Something went wrong....Retrying");
+      }
+
+      const data = await response.json();
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+      setIsTrying(true);
+      timeoutID = setTimeout(()=>{
+        fetchMovieHandler();
+      },3000)
+    }
     setIsLoading(false);
   }
+ 
+  
+  let content = <p>Found no movies.</p>;
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+  if (error) {
+    content = <p>{error}</p>;
+  }
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  } 
+  const cancelHandler = () => {
+    clearTimeout(timeoutID);
+    setIsTrying(false);
+    setError(null);
+  }
+
   return (
     <React.Fragment>
       <section>
+        {isTrying && <button onClick={cancelHandler}>Stop retrying</button>}
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies..</p>}
-        {isLoading && <p>Loading..</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
