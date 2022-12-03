e32eb0e738c642283fdb7af94219f49267403d9b
Ayush Sagar <70971035+itsmeAYUSH@users.noreply.github.com>
2022-12-04 00:02:30 +0530

    Add files via upload
    use of useEffect and optimized code

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
+let timeoutID;
 function App() {
-  // const dummyMovies = [
-  //   {
-  //     id: 1,
-  //     title: 'Some Dummy Movie',
-  //     openingText: 'This is the opening text of the movie',
-  //     releaseDate: '2021-05-18',
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'Some Dummy Movie 2',
-  //     openingText: 'This is the second opening text of the movie',
-  //     releaseDate: '2021-05-19',
-  //   },
-  // ];
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [isTrying , setIsTrying ] = useState(false);
-  let timeoutID;
+  const [isTrying, setIsTrying] = useState(false);
 
-  async function fetchMovieHandler() {
+  const fetchMovieHandler = useCallback(async () => {
+
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.py4e.com/api/film/");
+      const response = await fetch("https://swapi.py4e.com/api/films/");
 
       if (!response.ok) {
         throw new Error("Something went wrong....Retrying");
@@ -47,14 +34,17 @@ function App() {
     } catch (error) {
       setError(error.message);
       setIsTrying(true);
-      timeoutID = setTimeout(()=>{
+      timeoutID = setTimeout(() => {
         fetchMovieHandler();
-      },3000)
+      }, 3000);
     }
     setIsLoading(false);
-  }
- 
-  
+  },[]);
+
+  useEffect(()=>{
+    fetchMovieHandler();
+  },[fetchMovieHandler]);
+
   let content = <p>Found no movies.</p>;
   if (movies.length > 0) {
     content = <MoviesList movies={movies} />;
@@ -64,18 +54,18 @@ function App() {
   }
   if (isLoading) {
     content = <p>Loading...</p>;
-  } 
+  }
   const cancelHandler = () => {
     clearTimeout(timeoutID);
     setIsTrying(false);
     setError(null);
-  }
+  };
 
   return (
     <React.Fragment>
       <section>
-        {isTrying && <button onClick={cancelHandler}>Stop retrying</button>}
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
+        {isTrying && <button onClick={cancelHandler}>Stop retrying</button>}
       </section>
       <section>{content}</section>
     </React.Fragment>
